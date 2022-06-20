b570d58050d448ed6c41bb745b21744f6245468e
Md Rubel Rana <rubel.rana@northsouth.edu>
2022-06-20 17:58:20 +0600

    mobile menu added

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       },
       boxShadow: {
         'custom-3px': '3px 0 5px #f5f5f5',
+        'custom-10px': '0 0 10px rgb(0 0 0 / 8%)',
       },
       fontFamily: {
         'poppins': ['Poppins'],

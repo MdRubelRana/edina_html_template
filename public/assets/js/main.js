633563e18a20e566df221de3f79e0e903c8bfd44
Md Rubel Rana <rubel.rana@northsouth.edu>
2022-06-21 13:30:53 +0600

    testimonial added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$(".owl-carousel").owlCarousel({
+	$(".my_carousel").owlCarousel({
 		autoplay: true,
 		lazyLoad: true,
 		loop: true,
@@ -24,6 +24,35 @@ $(document).ready(function () {
 
 			1366: {
 				items: 3
+			}
+		}
+	});
+
+});
+
+// Testimonials Carousel
+$(document).ready(function () {
+	$(".testimonial__carousel").owlCarousel({
+		autoplay: true,
+		lazyLoad: true,
+		loop: true,
+		margin: 30,
+		responsiveClass: true,
+		autoHeight: true,
+		autoplay: true,
+		smartSpeed: 600,
+		nav: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			1024: {
+				items: 2
 			}
 		}
 	});
